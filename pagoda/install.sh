e111db8ddf253603823829ce8439e1a529ae2f73
Tijn <me@imrat.com>
2012-04-11 10:41:31 +0100

    Improved logging

--- pagoda/install.sh
+++ pagoda/install.sh
@@ -1,17 +1,17 @@
 #! /bin/bash
 # get latest version of code and prepare environment
 echo Getting latest version of TimeTabs...
-wget -a log.txt -q -t 3 --progress=dot http://tabedit.org/timetabs/timetabs-latest.gz
+wget -a log.txt -t 3 --progress=dot http://tabedit.org/timetabs/timetabs-latest.gz
 
 echo Unpacking TimeTabs...
-tar -xzf timetabs-latest.gz
+tar -xzf timetabs-latest.gz >> log.txt
 
 echo Moving config and default files...
-mv pagoda/config.php config.php
-mv pagoda/default pages
+mv pagoda/config.php config.php >> log.txt
+mv pagoda/default pages >> log.txt
 
 echo Cleaning up files...
-rm timetabs-latest.gz
-rm -r pagoda
+rm timetabs-latest.gz >> log.txt
+rm -r pagoda >> log.txt
 
 echo Done! 
