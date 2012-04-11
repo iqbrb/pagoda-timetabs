2fbd3815c1efd47d067ee466770e8c94bf8bc930
Tijn <me@imrat.com>
2012-04-11 12:21:39 +0100

    changed default creation

---
+++ pagoda/deploy.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+# get latest version of code and prepare environment
+echo Getting latest version of TimeTabs...
+curl -L http://dl.dropbox.com/u/36253922/timetabs-latest.tar.gz | tar zx
+
+echo Moving app files...
+cd tijn72*
+mv * ..
+cd ..
+
+echo Moving config file...
+mv pagoda/config.php config.php
+
+echo Cleaning up files...
+rm -r tijn72*
+
+echo Done! 
--- pagoda/install.sh
+++ pagoda/install.sh
@@ -1,21 +1,6 @@
 #! /bin/bash
 # get latest version of code and prepare environment
-echo Getting latest version of TimeTabs...
-curl -L http://dl.dropbox.com/u/36253922/timetabs-latest.tar.gz | tar zx
-
-echo Moving app files...
-cd tijn72*
-mv * ..
-cd ..
-
-echo Moving config file...
-mv pagoda/config.php config.php
-
 echo Creating default page
-mkdir pages/default
-mv default/* pages/default
-
-echo Cleaning up files...
-rm -r tijn72*
+mv pagoda/default pages
 
 echo Done! 
