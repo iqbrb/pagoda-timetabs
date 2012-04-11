45e869ebfd38f8f929b591de4e0fb1ee3797450f
Tijn <me@imrat.com>
2012-04-11 10:33:29 +0100

    Wget command updated

--- pagoda/install.sh
+++ pagoda/install.sh
@@ -1,33 +1,17 @@
 #! /bin/bash
 # get latest version of code and prepare environment
-clear
-
-echo -en '\E[47;32m'"\033[1mS\033[0m"
 echo Getting latest version of TimeTabs...
-tput sgr0
+wget -a log.txt -q -t 3 --progress=dot http://tabedit.org/timetabs/timetabs-latest.gz
 
-wget http://tabedit.org/timetabs/timetabs-latest.gz
-
-echo -en '\E[47;32m'"\033[1mS\033[0m"
 echo Unpacking TimeTabs...
-tput sgr0
-
 tar -xzf timetabs-latest.gz
 
-echo -en '\E[47;32m'"\033[1mS\033[0m"
 echo Moving config and default files...
-tput sgr0
-
 mv pagoda/config.php config.php
 mv pagoda/default pages
 
-echo -en '\E[47;32m'"\033[1mS\033[0m"
 echo Cleaning up files...
-tput sgr0
-
 rm timetabs-latest.gz
 rm -r pagoda
 
-echo -en '\E[47;32m'"\033[1mS\033[0m"
 echo Done! 
-tput sgr0
