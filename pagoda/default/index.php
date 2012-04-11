51f116ccb813ae376f283bb3738cdb35418a3d2a
Tijn <me@imrat.com>
2012-04-11 09:41:52 +0100

    moved default file to boxfile move

---
+++ pagoda/default/index.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+	<title>Default TimeTabs Page</title>
+	<style>
+		html,body {
+			margin:0px;
+			padding:0px;
+			height:100%;
+			background:#eee;
+			font-family:sans-serif;
+		}
+		#wrap {
+			width:810px;
+			margin:0 auto;
+			background:white;
+			height:100%;
+		}
+	</style>
+</head>
+<body>
+<div id="wrap">
+	<h1>Default TimeTabs Page</h1>
+	<p>This is an empty page. Use the file uploader to get your files uploaded.</p>
+
+	<p>Remember though -> keep your pages to 810 pixels wide!</p>
+	<span style="width:810px;height:50px; background:#75C9FF; color:white;font-weight:bold;font-size:20px; display:block;text-align:center;line-height:50px;">This square is exactly 810px by 50px</span>
+</div>
+</body>
+</html>
